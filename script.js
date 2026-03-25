387ed8dafc6a517bb66871f9d67f14d7bc6e8481
Askley Cachuela <cachuelaaskley4@gmail.com>
2026-03-25 15:46:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Portfolio Website Loaded!");
+const text = [
+"BSIT Student",
+"Future Software Developer",
+"Data & System Builder",
+"Learning Every Day 🚀"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+        setTimeout(type,1500);
+    } else {
+        setTimeout(type,80);
+    }
+})();
